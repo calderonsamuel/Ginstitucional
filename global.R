24ff5b7c812cead65e549d7d5ef69af692235a87
calderonsamuel <samuel.calderon@uarm.pe>
2020-08-12 13:53:18 -0500

    change geom_bar to geom_col

--- global.R
+++ global.R
@@ -10,15 +10,24 @@ my_plot <- function(df, columna, separate = FALSE, orientation = "x"){
   if (separate == TRUE) {
     data <- 
       separate_rows(data, x, sep = ", ") %>% 
-      mutate(x = str_trunc(x, 60)) %>% 
-      filter(x != "NA")
+      mutate(x = str_trunc(x, 60)) 
   }
-  p <- ggplot(data, aes(fill = x))
+  data <- data %>% 
+    filter(x != "NA") %>% 
+    group_by(x) %>% 
+    tally(sort = TRUE, name = "recuento") %>% 
+    mutate(x = as_factor(x))
+  
+  p <- ggplot(data)
   
   if (orientation == "x"){
-    p + geom_bar(aes(x = fct_infreq(x)))
+    p + 
+      geom_col(aes(x = x, y = recuento, fill = x)) +
+      geom_label(aes(x = x, y = recuento, label = recuento))
   }  else if (orientation == "y"){
-    p + geom_bar(aes(y = fct_infreq(x)))
+    p + 
+      geom_col(aes(y = x, x = recuento, fill = x)) +
+      geom_label(aes(y = x, x = recuento, label = recuento))
   }
   
 }
