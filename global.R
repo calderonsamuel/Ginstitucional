a519cfd7905dac5ab0e3384ee81f81ff087e415e
calderonsamuel <samuel.calderon@uarm.pe>
2020-08-12 17:43:22 -0500

    separate my_pipe from my_plot

--- global.R
+++ global.R
@@ -5,7 +5,8 @@ mi_pivot <- function(.df, .str){
     filter(value != "")
 }
 
-my_plot <- function(df, columna, separate = FALSE, orientation = "x"){
+my_pipe <- function(df, columna, separate = FALSE){
+  
   data <- select(df, x = contains(columna))
   if (separate == TRUE) {
     data <- 
@@ -17,19 +18,32 @@ my_plot <- function(df, columna, separate = FALSE, orientation = "x"){
     group_by(x) %>% 
     tally(sort = TRUE, name = "recuento") %>% 
     mutate(x = as_factor(x))
+  data
+}
+
+my_graph <- function(df, orientation = "x"){
+  ggplot(data, aes(x = x, recuento)) +
+    geom_col(aes(fill = x)) +
+    geom_label(aes(label = recuento))
+}
+
+my_plot <- function(df, orientation = "x", paleta){
   
-  p <- ggplot(data)
+  p <- ggplot(df)
   
   if (orientation == "x"){
-    p + 
+    p <- p + 
       geom_col(aes(x = x, y = recuento, fill = x)) +
       geom_label(aes(x = x, y = recuento, label = recuento))
   }  else if (orientation == "y"){
-    p + 
+    p <- p + 
       geom_col(aes(y = x, x = recuento, fill = x)) +
       geom_label(aes(y = x, x = recuento, label = recuento))
   }
-  
+  p +
+    my_wrap() + 
+    scale_fill_brewer(palette = paleta) +
+    theme(legend.position = "none")
 }
 
 my_wrap <- function(..., width = 35){
